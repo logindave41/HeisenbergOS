158a8d5fe98b411ff0ea5403a5f771d2c1bce04f
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-12-07 14:41:39 -0200

    Código preliminar para modo protegido.

--- src/boot/loader.asm
+++ src/boot/loader.asm
@@ -1,9 +1,11 @@
+;-----------------------------------------------------------------
 ; Este é o código que começa a inicialização da carga do kernel...
-bits 16
+;-----------------------------------------------------------------
 
-;------------------------
-; Neste ponto temos certeza que DS=CS=0x0060 e a pilha está no topo da memória RAM baixa...
-;------------------------
+; Alguns macros para facilitar nossa vida.
+%include "macros.inc"
+
+bits 16
 
 ;------------------------
 ; Liga e verifica o sinal gateA20.
@@ -23,47 +25,111 @@ enableA20:
 ; Testa se o gateA20 foi habilitado.
 ;--------------------------
 testA20:
-  push  ds
+  mov   cx,ds       ; Guarda DS.
 
   xor   ax,ax
   mov   ds,ax
   dec   ax
   mov   es,ax
 
+  ; Com gateA20 desligado, 0x0000:0x0510 e 0xFFFF:0x0520 
+  ; apontam para o mesmo lugar!
   mov   si,0x510
   mov   di,0x520
 
-  ; Salva os valores obtidos nos segmentos.
+  ; Salva os valores contidos nos segmentos.
   mov   bl,[si]
   mov   bh,[es:di]
 
+  ; Escreve valores conhecidos em ambos os lugares.
   mov   byte [si],0
   mov   byte [es:di],0xff
 
+  ; Se gateA20 estiver ligdo, 0x0000:0x0510 contém 0,
+  ; senão, conterá 0xff.
   cmp   byte [si],0
-  ; ZF=1 se gateA20 estiver funcionando!
+  ; Neste ponto ZF=1 se gateA20 estiver funcionando!
 
   ; Recupera os valores originais.
   mov   [es:di],bh
   mov   [si],bl
 
-  pop   ds
-  jnz   error_enabling_a20
+  mov   ds,cx       ; Recupera DS.
 
-  hlt     ; temporário, enquanto não codifico coisa alguma, ainda...
+  jz    prepare_protected_mode
 
-error_enabling_a20:
+  ; Erro ao testar o gate A20...
   mov   si,error_enabling_a20_str
   call  puts
-  hlt
+  jmp   halt
 error_enabling_a20_str:
   db    "Error enabling gate A20!", 13, 10, 0
 
 ;------------------------
+; Prepara o ambiente para entrarmos
+; no modo protegido. Esse pedaço ainda é prelimiar!
+;------------------------
+prepare_protected_mode:
+  ;*** É interessante desabilitar NMIs aqui também?
+  cli
+  lgdt  [gdtptr]
+  lidt  [idtptr]
+  ;*** Precisa carregar Task Register aqui?! Acho que sim!
+
+  ; Habilita o bit PE de CR0.
+  mov   eax,cr0
+  or    eax,1
+  mov   cr0,eax
+
+  ; Salta para o modo protegido!
+  jmp   _CODE32SEG:_PTR(protected_mode_entry)
+
+;--------------------------
+; Estruturas usadas por LGDT, LIDT (e LTR?).
+;--------------------------
+gdtptr:
+  dw    gdt_end - gdt     ; Tamanho da tabela.
+  dd    _PTR(gdt)         ; Endereço físico da tabela.
+
+; Por enquanto não criei ainda uma tabela de vetores interrupção!
+idtptr:
+  dw    0
+  dd    0
+
+;--------------------------
+; Tabela de descritores globais simples.
+;--------------------------
+  align 16
+gdt:
+  GDT_ENTRY 0,0,0               ; Null descriptor
+  GDT_ENTRY 0,0xfffff,0xc9a     ; Kernel Codeseg descriptor
+  GDT_ENTRY 0,0xfffff,0xc92     ; Kernel Dataseg descriptor
+gdt_end:
+
+;******************************************************************************
+;------------------------
 ; Aqui começa a inicialização do modo protegido.
 ; Nosso trabalho aqui é deixar o ambiente pronto para a carga do kernel
-; à partir do endereço físico 0x100000 (logo acima do primeiro megabyte de memória).
-; Mas é só o começo... o ambiente vai ser ajustado mesmo é pelo código do kernel!
-; A partir deste ponto não podemos usar quaisquer rotinas da BIOS!
+; à partir do endereço físico 0x100000 (logo acima do primeiro megabyte de 
+; memória). Mas é só o começo... o ambiente vai ser ajustado mesmo é pelo código
+; do kernel! A partir deste ponto não podemos usar quaisquer rotinas da BIOS!
+;
+; Graças ao salto para o modo protegido, os ponteiros agora enxergam toda a
+; memória de maneira linear (até 4 GB)... Só precisamos ajustar os seletores.
 ;------------------------
 bits 32
+
+  align 16
+protected_mode_entry:
+  ; ajusta os seletores: DS=ES=FS=GS=SS=DATA32SEG
+  mov   eax,_DATA32SEG
+  mov   ds,ax
+  mov   es,ax
+  mov   fs,ax
+  mov   gs,ax
+  mov   ss,ax
+  mov   esp,0x9fffc     ; Continuamos com SS:ESP apontando para uma pilha na
+                        ; memória inferior!
+  sti
+
+  hlt   ; continua...
--- src/boot/mbr.asm
+++ src/boot/mbr.asm
@@ -2,7 +2,7 @@
 ;
 ; Embora eu tenha chamado esse arquivo de mbr.asm, ele não implementa a MBR.
 ; Preferi usar um boot manager como o GRUB para "bootar" a partição correta.
-; Essa listagem implementa o loader do kernel.
+; Essa listagem implementa o bootsector e o loader do kernel.
 ;
 bits 16
 
@@ -10,8 +10,9 @@ bits 16
 
 ;----------------------------
 ; Quando a BIOS carrega a MBR ela o coloca no endereço 0x0000:0x7c00.
-; Note que o segmento é 0... Isso permite que o código acesso a área
-; da BIOS sem muito esforço, no entanto, causa alguns problema para nós...
+; Note que o segmento é 0... Isso permite que o código acesse a área
+; da BIOS (0x0040:0) sem muito esforço, no entanto, causa alguns problema para 
+; nós...
 ;
 ; O "segmento" de todo programa ASM standalone, que não é especificado
 ; no script do linker, começa sempre no endereço 0, não em 0x7c00. Assim,
@@ -25,18 +26,29 @@ _start:
 ; Região de dados
 ;-----------------------------
 
-                        ; Temos essa assinatura aqui porque alguns valores serão preenchidos
-                        ; por um utilitário externo. Precisamos dela para localizar esses valores no
-                        ; arquivo binário...
-                        dd  0x0B16B00B5   ; Ahhh "peitões"! :)
-cylinder:               dw  0     ; valor default, preenchido depois.
-head:                   db  0     ; valor default, preenchido depois.
-sector                  db  1     ; valor default, preenchido depois.
+          ; Temos essa assinatura aqui porque alguns valores serão
+          ; preenchidos por um utilitário externo. Precisamos dela
+          ; para localizar esses valores no arquivo binário...
+          dd  0x0B16B00B5   ; Ahhh "peitões"! :)
 
-num_sectors_after_mbr:  db  1 + ((_end - loader) / 512)   ; *** Isto está correto? ***
+          ; Os números do cilindro, cabeça e setor serão colocados
+          ; nessas posições por um utilitário externo.
+cylinder: dw  0     ; valor default, preenchido depois.
+head:     db  0     ; valor default, preenchido depois.
+sector:   db  1     ; valor default, preenchido depois.
+
+drive_no: db  0x80  ; Drive default é o primeiro HD, mas isso será
+                    ; fornecido pela BIOS durante o boot!
+
+          ; Nosso loader tem tamanho de múltiplo exato de 512 bytes!
+          ; Com isso, precisamos apenas calclar o número de setores gastos.
+num_sectors_after_mbr:  db  (_end - loader) / 512
+
+          ; Strings
 loading_str:            db  "Loading Heisenberg OS...",13,10,0
 disk_read_error_str:    db  "Error reading loader. System halted!", 13, 10, 0
 
+; O código do setor de boot começa aqui!
 boot_start:
   mov   ax,cs
   mov   ds,ax         ; Ajusta DS para acessar os mesmos dados deste segmento.
@@ -62,29 +74,34 @@ boot_start:
 ; Note que todo esse código não pode ultrapassar uns 8 kB se você pretende
 ; que ele funcione num diskete de 1.44 MB (18 setores por trilha).
 ;----------------------------
-  cld
+  cld                           ; Certifica-se que SI ou DI sejam incrementados.
   mov   ax,_MBRSEG
   mov   es,ax
   xor   si,si
   mov   di,si
   mov   cx,256
   rep   movsw
-  jmp   _MBRSEG:mbr_real_start
+  jmp   _MBRSEG:mbr_real_start  ; Salta para a nova cópia.
 
 mbr_real_start:
   mov   ds,ax       ; Ajusta DS.
+
+  mov   [drive_no],dl ; Guarda o nº do drive fornecido pela BIOS.
 
   ; Mostra a string "Loading Heisenberg OS...".
   mov   si,loading_str
   call  puts
 
   ; Usa a BIOS para carregar o loader...
+  ; É necessário adequar o valor de CX...
   mov   ax,ds
   mov   es,ax
   mov   bx,loader
   call  chs_cylinder_sector_encode
   mov   al,[num_sectors_after_mbr]
   mov   dh,[head]
+  ; OBS: A BIOS fornece o drive em DL!
+  ;      Note que não mexemos com DL até agora!
   int   0x13
   jnc   disk_read_ok
 
@@ -94,18 +111,22 @@ error_loading_loader:
   call  puts
 
   ; Pára tudo e deixa parado (mesmo se ouver uma interrupção!).
-.halt:
-  cli
+halt:
+  cli           ; Desabilita interrupções.
   hlt
-  jmp   .halt  
+  jmp   halt    ; Isto é apenas uma precaução... HLT "pára" o processador,
+                ; mas uma interrupção NMI pode tirá-lo desse estado de
+                ; "sonolência"... Não quero ter que mascarar NMIs aqui, agora.
 
 disk_read_ok:
+  ; Calcula e verifica o checksum do loader.
   mov   si,loader
   mov   cx,loader_cksum - loader
   call  calc_cksum
   and   ax,[loader_cksum]
   jnz   error_loading_loader
 
+  ; Se tudo deu certo, salta para o loader.
   jmp   loader
 
 ;----------------------------
@@ -133,13 +154,15 @@ puts:
 ;          CX tamanho do bloco em bytes.
 ; Saída:   AX crc.
 ;
-; Rotina equivalente em C (desconsiderando os carry-outs):
+; Rotina equivalente (só que menor!) em C:
 ;
 ;   short calc_cksum(char *buffer, size_t size)
 ;   {
-;      short sum = 0;
+;      unsigned short sum = 0;
+;
 ;      while (size--)
-;        sum += *buffer++;
+;        sum += *(unsigned char *)buffer++;
+;      if (sum >> 16) sum = ((sum >> 16) + sum) & 0xffff;
 ;      return ~sum;
 ;   }
 ;----------------------------
@@ -170,7 +193,8 @@ chs_cylinder_sector_encode:
   mov   cx,[cylinder]
   and   cx,0x3ff          ; Zera bits superiores de CX.
   rol   cx,8              ; CL contém parte superior e CH a inferior.
-  rol   cl,6              ; Coloca os 2 bits superiores do cilindro na parte superior de CL.
+  rol   cl,6              ; Coloca os 2 bits superiores do cilindro na parte 
+                          ; superior de CL.
   mov   al,[sector]
   and   al,0x1f           ; Zera a parte superior do setor.
   or    cl,al             ; mistura CL com AL.
@@ -188,6 +212,7 @@ chs_cylinder_sector_encode:
   times 510 - ($ - $$) db 0
 magic:
   db  0x55, 0xaa
+;******************************************************************************
 ;--------------------
 ; O codigo e dados contidos no restante dessa listagem pertencem aos
 ; setores adjacentes ao MBR...
@@ -202,11 +227,18 @@ loader:         ; Início do loader.
 
 ;----------------------------
 ; Mesmo artifício usado para o final do setor de boot,
-; Só que, agora, garante que nosso arquivo terá tamanho exato de múltiplos de 512.
+; Só que, agora, garante que nosso arquivo terá tamanho exato de múltiplos de 
+; 512 bytes.
 ;----------------------------
   times 510 - (($ - $$) % 512) db 0
 loader_cksum: dw    0     ; Apenas um checksum para determinar se
                           ; o código do MBR conseguiu ler todos os setores
                           ; corretamente. Vai ser preenchido por um utilitário
                           ; externo depois...
+
+; Quero que esse loader (mais o bootsector) tenham, no máximo, 8 kB.
+; Ou seja, 16 setores.
+%if ($ - $$) > 8192
+  %error "Loader muito grande!"
+%endif
 _end:           ; Fim do loader.
