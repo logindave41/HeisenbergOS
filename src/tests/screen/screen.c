6457e0d910aac1c45de6ceb798583eb6f615adba
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-12-07 20:58:57 -0200

    Acertos no teste.

--- src/tests/screen/screen.c
+++ src/tests/screen/screen.c
@@ -1,7 +1,7 @@
-#define VIDEO_BUFFER_PTR 0xb8000
+#define VIDEO_BUFFER_PTR (void *)0xb8000
 
-#define SCREEN_WIDTH  80
-#define SCREEN_HEIGHT 25
+#define SCREEN_WIDTH  80U
+#define SCREEN_HEIGHT 25U
 
 extern unsigned char screen_x;
 extern unsigned char screen_y;
@@ -9,7 +9,7 @@ extern unsigned char screen_attrib;
 
 extern void memcpy(void *, void *, unsigned int);
 
-void __attribute__((regparms(0))) gotoxy(unsigned int x, unsigned int y)
+void __attribute__((noinline)) gotoxy(unsigned int x, unsigned int y)
 {
   if (x >= SCREEN_WIDTH) x = SCREEN_WIDTH - 1;
   if (y >= SCREEN_HEIGHT) y = SCREEN_HEIGHT - 1;
@@ -17,7 +17,7 @@ void __attribute__((regparms(0))) gotoxy(unsigned int x, unsigned int y)
   screen_y = y;
 }
 
-void __attribute__((regparams(0))) putchar(unsigned char c)
+void __attribute__((noinline)) putchar(unsigned char c)
 {
   unsigned char *p;
 
@@ -26,7 +26,7 @@ void __attribute__((regparams(0))) putchar(unsigned char c)
     case '\r':  gotoxy(screen_x, screen_y+1); break;
     case '\n':  gotoxy(0, screen_y); break;
     default:
-      p = VIDEO_BUFFER_PTR + ((unsigned int)screen_x * 2 + (unsigned int)screen_y * 160);
+      p = (unsigned char *)VIDEO_BUFFER_PTR + ((unsigned int)screen_x * 2 + (unsigned int)screen_y * SCREEN_WIDTH*2);
       *p++ = c;
       *p = screen_attrib;
       if (++screen_x >= SCREEN_WIDTH)
@@ -37,14 +37,14 @@ void __attribute__((regparams(0))) putchar(unsigned char c)
 
       if (screen_y > SCREEN_HEIGHT)
       {
-        unsigned size = (screen_y - SCREEN_HEIGHT - 1)*SCREEN_WIDTH*2;
-        unsigned char *p = VIDEO_BUFFER_PTR;
-        unsigned char *q = VIDEO_BUFFER_PTR + size;
+        unsigned size = (screen_y - SCREEN_HEIGHT - 1)*SCREEN_WIDTH;
+        unsigned short *p = (unsigned short *)VIDEO_BUFFER_PTR;
+        unsigned short *q = p + size;
         unsigned short ca = ((unsigned short)' ' | ((unsigned short)screen_attrib << 8));
 
         memcpy(p, q, SCREEN_HEIGHT*SCREEN_WIDTH*2 - size);
-        p += (SCREEN_HEIGHT - 1)*SCREEN_WIDTH*2;
-        q = p + SCREEN_WIDTH*2;
+        p += (SCREEN_HEIGHT - 1)*SCREEN_WIDTH;
+        q = p + SCREEN_WIDTH;
         while (p <= q) *p++ = ca;
 
         screen_y = SCREEN_HEIGHT - 1;
