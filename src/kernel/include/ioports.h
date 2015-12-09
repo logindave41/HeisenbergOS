b94c23805630e3a8edad83983203ff4a6623ff76
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-12-09 14:56:24 -0200

    Pequena correção em inw().

--- src/kernel/include/ioports.h
+++ src/kernel/include/ioports.h
@@ -3,32 +3,38 @@
 
 #define outb(port,data) \
   __asm__ __volatile__ ( \
-  "outb %1,%%dx" \
-  : : "dN" ((unsigned short)(port)), "a" ((unsigned char)(data)) \
+    "outb %%al,%0" \
+    : : "dN" ((unsigned short)(port)), "a" ((unsigned char)(data)) \
    );
 
 #define outw(port,data) \
   __asm__ __volatile__ ( \
-  "outw %1,%%dx" \
-  : : "dN" ((unsigned short)(port)), "a" ((unsigned short)(data)) \
+    "outw %%ax,%0" \
+    : : "dN" ((unsigned short)(port)), "a" ((unsigned short)(data)) \
    );
 
-#define inb(port) \
-  ({ unsigned char tmp; \
-  \
-    __asm__ __volatile__ ( \
-    "inb %%dx,%%al" \
-     : "=a" (tmp) : "dN" ((unsigned short)(port)) \
-      ); \
-    tmp; })
+unsigned char inline inb(unsigned short port)
+{
+  unsigned char tmp;
 
-#define inw(port) \
-  ({ unsigned short tmp; \
-  \
-    __asm__ __volatile__ ( \
-    "inb %%dx,%%ax" \
-     : "=a" (tmp) : "dN" ((unsigned short)(port)) \
-      ); \
-    tmp; })
+  __asm__ __volatile__ (
+    "inb %1,%%al"
+     : "=a" (tmp) : "dN" (port)
+  );
+
+  return tmp; 
+}
+
+unsigned short inline inw(unsigned short port)
+{ 
+  unsigned short tmp;
+
+  __asm__ __volatile__ (
+    "inw %1,%%ax"
+     : "=a" (tmp) : "dN" (port)
+  );
+
+  return tmp; 
+}
     
 #endif
