5ab17626820cdef232589fdefc29c33edde5361b
Frederico Lamberti Pissarra <fredericopissarra@gmail.com>
2015-12-11 13:11:27 -0200

    Apenas um pequenino acerto.

--- src/kernel/include/ioports.h
+++ src/kernel/include/ioports.h
@@ -2,15 +2,15 @@
 #define __IOPORTS_INCLUDED__
 
 #define outportb(port, data) \
-  __asm__ __volatile__ ( "outb %%al,%%dx" : : "d" ((unsigned char)(port)), "a" ((data)) )
+  __asm__ __volatile__ ( "outb %%al,%%dx" : : "d" ((unsigned char)(port)), "a" ((unsigned char)(data)) )
 
 #define outportw(port, data) \
-  __asm__ __volatile__ ("outw %%ax,%%dx" : : "d" ((unsigned short)(port)), "a" ((data)) )
+  __asm__ __volatile__ ("outw %%ax,%%dx" : : "d" ((unsigned short)(port)), "a" ((unsigned short)(data)) )
 
 #define inportb(port) \
-  ({ unsigned char tmp; __asm__ __volatile__ ("inb %%dx,%%al" : "=a" (tmp) : "d" ((port))); tmp; })
+  ({ unsigned char tmp; __asm__ __volatile__ ("inb %%dx,%%al" : "=a" (tmp) : "d" ((unsigned short)(port))); tmp; })
 
 #define inportw(port) \
-  ({ unsigned short tmp; __asm__ __volatile__ ("inb %%dx,%%ax" : "=a" (tmp) : "d" ((port))); tmp; })
+  ({ unsigned short tmp; __asm__ __volatile__ ("inb %%dx,%%ax" : "=a" (tmp) : "d" ((unsigned short)(port))); tmp; })
 
 #endif
